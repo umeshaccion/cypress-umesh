8ff0ebd10f9cbd9b1c3ed9382c78c00fdd2e840d
umeshaccion <umesh.belge@accionlabs.com>
2023-12-19 10:16:21 +0000

    DEMO-1 Changes updated by GEN AI tool.

---
+++ cypress/e2e/API_TESTING/stores/getOrderById.cy.js
@@ -0,0 +1,42 @@
+import Ajv from 'ajv';
+const ajv = new Ajv();
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+let requestInfo = JSON.parse(
+    JSON.stringify({ url: '/store/order/{orderId}', method: 'GET' })
+);
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Find purchase order by ID', () => {
+    it('For valid response try integer IDs with value >= 1 and <= 10. Other values will generated exceptions', () => {
+        cy.fixture('200_application_json__getOrderById').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+});
