1187f2e1dfff12dae0a28c5c9087d816fdff2477
umeshaccion <umesh.belge@accionlabs.com>
2023-12-27 05:39:21 +0000

    CS-2 Changes updated by GEN AI tool.

--- cypress/e2e/API_TESTING/pets/pet_petId_uploadImage.cy.js
+++ cypress/e2e/API_TESTING/pets/pet_petId_uploadImage.cy.js
@@ -6,49 +6,55 @@ let requestInfo = JSON.parse(
 );
 requestInfo.url = apiBaseURL + requestInfo.url;
 
-describe('uploads an image Yes ', () => {
-    it('/pet/{petId}/uploadImage', () => {
+describe('uploads an image', () => {
+    // Please be informed that the API in this file has been deprecated and will no longer be supported.
+    it.skip('/pet/{petId}/uploadImage', () => {
         cy.fixture(
-            '200_application_json_application_json_pet_petId_uploadImage'
+            '200_application_json_multipart_form-data_pet_petId_uploadImage'
         ).then((fixtureResponse) => {
-            requestInfo.body = fixtureResponse.payload
-                ? fixtureResponse.payload
-                : '';
-            requestInfo.headers = fixtureResponse.headers
-                ? fixtureResponse.headers
-                : '';
-            let pathParams = fixtureResponse.pathParam
-                ? fixtureResponse.pathParam
-                : '';
-            for (const key in pathParams) {
-                if (pathParams.hasOwnProperty(key)) {
-                    const placeholder = '{' + key + '}';
-                    requestInfo.url = requestInfo.url.replace(
-                        new RegExp(placeholder, 'g'),
-                        pathParams[key]
-                    );
+            cy.fixture('**filePath**').then((fileContent) => {
+                const blob = new Blob([fileContent], {
+                    type: requestInfo.headers['Content-Type'],
+                });
+                const formData = new FormData();
+                formData.append('file', blob);
+                requestInfo.body = formData;
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                let pathParams = fixtureResponse.pathParam
+                    ? fixtureResponse.pathParam
+                    : '';
+                for (const key in pathParams) {
+                    if (pathParams.hasOwnProperty(key)) {
+                        const placeholder = '{' + key + '}';
+                        requestInfo.url = requestInfo.url.replace(
+                            new RegExp(placeholder, 'g'),
+                            pathParams[key]
+                        );
+                    }
                 }
-            }
-            requestInfo.qs = fixtureResponse.queryParam
-                ? fixtureResponse.queryParam
-                : '';
-            requestInfo.cookies = fixtureResponse.cookie
-                ? fixtureResponse.cookie
-                : '';
-            cy.request(requestInfo).then((response) => {
-                expect(response.status).to.eq(
-                    parseInt(fixtureResponse.responseStatusCode)
-                );
-                if (
-                    fixtureResponse.responseSchema &&
-                    fixtureResponse.responseSchema != ''
-                ) {
-                    const validate = ajv.compile(
-                        fixtureResponse.responseSchema
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+                requestInfo.cookies = fixtureResponse.cookie
+                    ? fixtureResponse.cookie
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
                     );
-                    const isValid = validate(response.body);
-                    expect(isValid).to.be.true;
-                }
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
             });
         });
     });
