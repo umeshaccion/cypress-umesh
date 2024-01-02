455b13c0fe901cddf5b0bfb50f0266f5620e86ca
umeshaccion <umesh.belge@accionlabs.com>
2024-01-02 13:51:02 +0000

    CS-18 Changes updated by GEN AI tool.

---
+++ cypress/e2e/API_TESTING/pets/addPet.cy.js
@@ -0,0 +1,89 @@
+import Ajv from 'ajv';
+const ajv = new Ajv();
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+let requestInfo = JSON.parse(JSON.stringify({ url: '/pet', method: 'POST' }));
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Add a new pet to the store', () => {
+    it('/pet', () => {
+        cy.fixture('200_application_json_application_json_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('/pet', () => {
+        cy.fixture('200_application_xml_application_json_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('/pet', () => {
+        cy.fixture('400__application_json_addPet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+});
