5b69156ef1c6b7fd95ca844e266db5caa193d3cf
umeshaccion <umesh.belge@accionlabs.com>
2024-01-03 10:24:16 +0000

    CS-22 CS-21 Changes updated by GEN AI tool.

---
+++ cypress/e2e/API_TESTING/pets/findPetsByTags.cy.js
@@ -0,0 +1,139 @@
+import Ajv from 'ajv';
+const ajv = new Ajv();
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+let requestInfo = JSON.parse(
+    JSON.stringify({ url: '/pet/findByTags', method: 'GET' })
+);
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Finds Pets by tags', () => {
+    // Please be informed that the API in this file has been deprecated and will no longer be supported.
+    it('successful operation', () => {
+        cy.fixture('200_application_json__findPetsByTags').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+    it('successful operation', () => {
+        cy.fixture('200_application_xml__findPetsByTags').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+    it('successful operation', () => {
+        cy.fixture('200_application_json__findPetsByTags').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+    it('successful operation', () => {
+        cy.fixture('200_application_xml__findPetsByTags').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+
+                requestInfo.qs = fixtureResponse.queryParam
+                    ? fixtureResponse.queryParam
+                    : '';
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+});
