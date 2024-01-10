235cdc60fb7a230096822ec21962527158ac3dcd
umeshaccion <umesh.belge@accionlabs.com>
2024-01-10 12:46:58 +0000

    CS-141 add all files for cypress which are generated by codGen

---
+++ cypress/e2e/API_TESTING/pet/addPet.cy.js
@@ -0,0 +1,255 @@
+import Ajv from 'ajv';
+const ajv = new Ajv();
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+const requestInfo = JSON.parse(JSON.stringify({ url: '/pet', method: 'POST' }));
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Add a new pet to the store', () => {
+    it('Successful operation', () => {
+        cy.fixture('200_application_xml_application_json_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_xml_application_xml_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture(
+            '200_application_xml_application_x-www-form-urlencoded_addPet'
+        ).then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_json_application_json_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_json_application_xml_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture(
+            '200_application_json_application_x-www-form-urlencoded_addPet'
+        ).then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid input', () => {
+        cy.fixture('405__application_json_addPet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid input', () => {
+        cy.fixture('405__application_xml_addPet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid input', () => {
+        cy.fixture('405__application_x-www-form-urlencoded_addPet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+});
---
+++ cypress/e2e/API_TESTING/pet/put__pet.cy.js
@@ -0,0 +1,417 @@
+import Ajv from 'ajv';
+const ajv = new Ajv();
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+const requestInfo = JSON.parse(JSON.stringify({ url: '/pet', method: 'PUT' }));
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Placeholder for Summary of test suite', () => {
+    // Deprecated API warning
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_xml_application_json_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_xml_application_xml_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture(
+            '200_application_xml_application_x-www-form-urlencoded_put__pet'
+        ).then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_json_application_json_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture('200_application_json_application_xml_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Successful operation', () => {
+        cy.fixture(
+            '200_application_json_application_x-www-form-urlencoded_put__pet'
+        ).then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid ID supplied', () => {
+        cy.fixture('400__application_json_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid ID supplied', () => {
+        cy.fixture('400__application_xml_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Invalid ID supplied', () => {
+        cy.fixture('400__application_x-www-form-urlencoded_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Pet not found', () => {
+        cy.fixture('404__application_json_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Pet not found', () => {
+        cy.fixture('404__application_xml_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Pet not found', () => {
+        cy.fixture('404__application_x-www-form-urlencoded_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+
+    it('Validation exception', () => {
+        cy.fixture('405__application_json_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Validation exception', () => {
+        cy.fixture('405__application_xml_put__pet').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            requestInfo.body = fixtureResponse.payload
+                ? fixtureResponse.payload
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+
+    it('Validation exception', () => {
+        cy.fixture('405__application_x-www-form-urlencoded_put__pet').then(
+            (fixtureResponse) => {
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                    if (
+                        fixtureResponse.responseSchema &&
+                        fixtureResponse.responseSchema != ''
+                    ) {
+                        const validate = ajv.compile(
+                            fixtureResponse.responseSchema
+                        );
+                        const isValid = validate(response.body);
+                        expect(isValid).to.be.true;
+                    }
+                });
+            }
+        );
+    });
+});
