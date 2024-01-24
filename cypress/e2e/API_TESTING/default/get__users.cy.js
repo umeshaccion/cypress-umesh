d2ef36cea350005ac22aae48b0d186e396d75a2d
umeshaccion <umesh.belge@accionlabs.com>
2024-01-24 11:12:52 +0000

    CS-231 add all files for cypress which are generated by codGen

---
+++ cypress/e2e/API_TESTING/default/get__users.cy.js
@@ -0,0 +1,31 @@
+import { ajv } from '../../../support/e2e';
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+const requestInfo = JSON.parse(
+    JSON.stringify({ url: '/users', method: 'GET' })
+);
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('Get a list of users', () => {
+    it('A list of users changed for update', () => {
+        cy.fixture('200__get__users').then((fixtureResponse) => {
+            requestInfo.headers = fixtureResponse.headers
+                ? fixtureResponse.headers
+                : '';
+            cy.request(requestInfo).then((response) => {
+                expect(response.status).to.eq(
+                    parseInt(fixtureResponse.responseStatusCode)
+                );
+                if (
+                    fixtureResponse.responseSchema &&
+                    fixtureResponse.responseSchema != ''
+                ) {
+                    const validate = ajv.compile(
+                        fixtureResponse.responseSchema
+                    );
+                    const isValid = validate(response.body);
+                    expect(isValid).to.be.true;
+                }
+            });
+        });
+    });
+});
